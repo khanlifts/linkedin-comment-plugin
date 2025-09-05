b86e226088c9b7d8a0ddae0b4d5490e5c110843a
Cyril Khan <cyrilkhan@users.noreply.github.com>
2025-09-05 16:23:40 +0200

    initial commit with Vue setup

---
+++ popup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="popup">
+    <h1>Hello from Vue + Plasmo</h1>
+  </div>
+</template>
+
+<script setup>
+// kannst du leer lassen oder Composables nutzen
+</script>
+
+<style scoped>
+.popup {
+  font-family: sans-serif;
+  padding: 1rem;
+}
+</style>
