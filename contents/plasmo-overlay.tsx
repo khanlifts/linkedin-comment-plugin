17d98cf60d55d8102c8ceacba7b084047795d272
Cyril Khan <cyrilkhan@users.noreply.github.com>
2025-09-25 17:45:40 +0200

    feat: add basic html switch component

--- contents/plasmo-overlay.tsx
+++ contents/plasmo-overlay.tsx
@@ -16,14 +16,12 @@ const PlasmoOverlay = () => {
   return (
     <div className="overlay">
       <h2 className="overlay__title">Feed Focus</h2>
-        <div className="overelay__switch-item">
-          <label className="overlay__label">Hide Notifications</label>
-        </div>
         <div className="overlay__switch-item">
-          <label className="overlay__label">Hide Messages</label>
-        </div>
-        <div className="overlay__switch-item">
-          <label className="overlay__label">Hide Feed</label>
+          <label className="overlay__switch">
+            <input className="overlay__switch-input" type="checkbox"/>
+            <span className="overlay__switch-slider overlay__switch-slider-round"></span>
+          </label>
+          <label className="overlay__label"></label>
         </div>
     </div>
   )
