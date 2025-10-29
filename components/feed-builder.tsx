93baba14dee22417b9ad4ecc823921782fb60747
Cyril Khan <cyrilkhan@users.noreply.github.com>
2025-10-29 14:59:15 +0100

    feat: add dynamic create list section

--- components/feed-builder.tsx
+++ components/feed-builder.tsx
@@ -1,16 +1,59 @@
+import { useState } from "react"
+
 const FeedBuilder = () => {
+  const [isCreating, setIsCreating] = useState(false)
+  const [listName, setListName] = useState("")
+
+  const handleCreateClick = () => {
+    if (listName.trim().length === 0) return
+    setListName("")
+    setIsCreating(false)
+  }
+
+  const handleCancelClick = () => {
+    setListName("")
+    setIsCreating(false)
+  }
+
   return (
     <div className="feed-builder">
       <div className="feed-builder__header">FeedBuilder</div>
       <div className="feed-builder__body">
-        <div className="feed-builder__create-list-section">
-          <input className="feed-builder__create-list-input"
-                 type="text"
-                 placeholder="Enter list name..."
-          />
-          <button className="feed-builder__create-list-button">Create</button>
-          <button className="feed-builder__create-list-cancel">Cancel</button>
-        </div>
+        {!isCreating ? (
+          <button
+            className="feed-builder__create-lists-button"
+            onClick={() => setIsCreating(true)}
+          >
+            + Create new list
+          </button>
+        ) : (
+          <div className="feed-builder__create-list-section">
+            <div className="feed-builder__create-list-section-input">
+              <input
+                className="feed-builder__create-list-input"
+                type="text"
+                placeholder="Enter list name..."
+                value={listName}
+                onChange={(e) => setListName(e.target.value)}
+              />
+            </div>
+            <div className="feed-builder__create-list-section-action">
+              <button
+                className="feed-builder__create-list-button"
+                disabled={listName.trim().length === 0}
+                onClick={handleCreateClick}
+              >
+                Create
+              </button>
+              <button
+                className="feed-builder__create-list-cancel"
+                onClick={handleCancelClick}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
